c02b1f2db5994941caaade071974ff22ea8fe690
Th G <thida.dev@gmail.com>
2021-02-06 19:31:38 +0100

    ajout heure dynamique + màj mineures

--- autocomplete.js
+++ autocomplete.js
@@ -73,33 +73,171 @@ function initMap(lat, lon, ville) {
         maxZoom: 20
     }).addTo(macarte);
 }
-window.onload = initMap(43.6769, 4.6287, 'Arles');  // au chargement de la page, je charge la map ciblée sur Arles par défaut
+initMap(43.6769, 4.6287, 'Arles');  // au chargement de la page, je charge la map ciblée sur Arles par défaut
 
 /* -------------------------------- /MAP --------------------------------*/
 
 /* -------------------------------- METEO --------------------------------*/
 
 const weatherDiv = document.querySelector('#weatherDiv');
-
+// fonction qui fetch l'API météo
 const weatherCall = (lat, lon, town) => {
     const urlWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&lang=fr&appid=39a119e15d4e7af2e13ccb87b4f2b2a4`;
     fetch(urlWeather)
     .then(response => response.json())
     .then((data) => {
-        console.log(data.current.weather[0].description);
+        //console.log(data.current.weather[0].description);
         weatherDiv.innerHTML = `<h3>${town}</h3>
-                                <p>La température est de ${data.current.temp}°C</p>
+                                <p>La température est de <strong>${data.current.temp}°C</strong></p>
                                 <p>Le ciel est : <img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png" /> ${data.current.weather[0].description}</p>`;
     })
 }
-window.onload = weatherCall(43.6769, 4.6287, 'Arles');
+weatherCall(43.6769, 4.6287, 'Arles');  // au chargement, météo d'Arles par défaut
 
 /* -------------------------------- /METEO --------------------------------*/
 
 /* -------------------------------- DATE --------------------------------*/
-const dateDuJour = new Date();
-const divDate = document.querySelector('#date');
-const annee   = dateDuJour.getFullYear();
-const mois    = ('0'+dateDuJour.getMonth()+1).slice(-2);
-const jour    = ('0'+dateDuJour.getDate()).slice(-2);
-divDate.innerHTML = `<strong>${jour}/${mois}/${annee}</strong>`;
+function runDate() {
+    const dateDuJour = new Date();
+    const divDate = document.querySelector('#date');
+    const annee   = dateDuJour.getFullYear();
+    const mois    = ('0'+dateDuJour.getMonth()+1).slice(-2);
+    const jour    = ('0'+dateDuJour.getDate()).slice(-2);
+    divDate.innerHTML = `<strong>${jour}/${mois}/${annee}</strong>`;
+}
+runDate();
+/* -------------------------------- /DATE --------------------------------*/
+
+/* -------------------------------- HEURE --------------------------------*/
+function runClock() {
+    let today   = new Date();
+    let hours   = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    let timeValue = hours;
+
+    // changement de la date automatique à minuit et 1 sec.
+    if(hours === 0 && minutes === 0 && seconds > 0){
+        runDate();
+    }
+
+    // Les deux prochaines conditions ne servent que pour l'affichage.
+    // Si le nombre de minutes est inférieur à 10, alors on ajoute un 0 devant...
+    // → fonction ternaire
+
+    timeValue += ((minutes < 10) ? ":0" : ":") + minutes;
+    timeValue += ((seconds < 10) ? ":0" : ":") + seconds;
+    document.querySelector('#heure').innerHTML = timeValue;
+    //timerRunning = true;
+}
+ 
+let timerID = setInterval(runClock,1000);
+/* -------------------------------- /HEURE --------------------------------*/
+
+/* --------------------------- PREVISIONS METEO ---------------------------*/
+
+// tableaux des icônes pour la météo jour/nuit
+const weatherDay = [];
+const weatherNight = ['<img src="http://openweathermap.org/img/wn/50n.png" />'];
+
+for (let i = 1; i < 14; i++) {  // boucle pour remplir les tableaux avec les icônes
+    if(i<10){       // pour mettre un zéro devant les nombres < 10
+        i = `0${i}`;
+    }
+    weatherDay.push(`<img src="http://openweathermap.org/img/wn/${i}d.png" />`);
+    weatherNight.push(`<img src="http://openweathermap.org/img/wn/${i}n.png" />`);
+}
+weatherDay.push('<img src="http://openweathermap.org/img/wn/50d.png" />');
+weatherNight.push('<img src="http://openweathermap.org/img/wn/50n.png" />');
+//console.log(weatherDay, weatherNight);
+
+const date = ['lundi', 'truc', 'bidule', 'lundi', 'truc', 'bidule'];    //TODO remplir avec API OpenWeatherMap !!
+const ctx = document.querySelector('#previsionsMeteo');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: date,
+        datasets: [{
+            label: '# of Votes',
+            data: weatherDay,//[12, 19, 3, 5, 2, 3], // rapport avec $date
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+/*
+var config = {
+    type: 'line',
+    data: {
+        labels: weatherDay,
+        datasets: [{
+            label: 'Prévisions météo',
+            borderColor: window.chartColors.red,
+            backgroundColor: window.chartColors.red,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart - Stacked Area'
+        },
+        tooltips: {
+            mode: 'index',
+        },
+        hover: {
+            mode: 'index'
+        },
+        scales: {
+            xAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Days'
+                }
+            }],
+            yAxes: [{
+                stacked: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Value'
+                }
+            }]
+        }
+    }
+};
+*/
